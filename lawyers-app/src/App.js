02a4e48701e845db0466ea1a3924f6f62cb8721e
hamad-SHE <eng_cis1@hotmail.com>
2019-01-16 17:10:44 +0300

    Show and profile

--- lawyers-app/src/App.js
+++ lawyers-app/src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-//import HomePage from './components/HomePage';
+import Show from './components/Show';
+import Profile from './components/Profile';
+//import ShowCases from './components/ShowCases';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      lowyers: [],
+      lawyersData: [],
       dashboard: [],
       dashboardLowyers: [],
-      lowyerName: ''
+      lawyerName: '',
+      displayProfile: false
     }
   }
 
   componentDidMount(){
+    this.fetchLawyers();
+    this.fetchDashData();
+  }
+
+  fetchLawyers() {
     const url = 'http://localhost:3000/lawyers';
     fetch(url)
     .then(response => response.json())
     .then(data => {
       console.log(data);
       this.setState({
-        lowyers: data
+        lawyersData: data
       })
     })
     .catch(error => {
@@ -28,15 +36,49 @@ class App extends Component {
     })
   }
 
+  fetchDashData() {
 
+  }
+
+  hendelName(name){
+    //console.log(name)
+    this.setState({
+      lawyerName: name,
+      displayProfile: true 
+    })
+  }
+
+  renderProfile(data){
+    // return data.map((el, index) => {
+    //   return el.lawyer === this.state.lawyerName ? <Profile lawyer={el} key={index} /> : ''
+    // })
+
+console.log(data); 
+
+
+    let lawyerProfile = data.filter( d => d.lawyer === this.state.lawyerName);
+    console.log("lawyerProfilelawyerProfile\n\n " , lawyerProfile); 
+    return (<Profile data={lawyerProfile }  /> )
+  }
+
+  renderShow(){
+
+    return  <Show hendelName={this.hendelName.bind(this)} />
+  }
+
+  /* renderShowCases(data){
+    return <ShowCases  />
+  } */
 
 
   render() {
     return (
       <div className="App">
-        {/* <HomePage/> */}
+      {this.state.displayProfile ? this.renderProfile(this.state.lawyersData) : this.renderShow() }
+        
+        
+        {/* {this.state.lawyerName !== '' ? this.renderProfile(this.state.lawyers): ''} */}
       </div>
-
     );
   }
 }
--- lawyers-app/src/components/Profile.js
+++ lawyers-app/src/components/Profile.js
@@ -1,61 +1,120 @@
 import React, { Component } from 'react';
+import ShowEdit from './ShowEdit';
 
 class Profile extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       showCases: true,
-      case: []
+      case: [],
+      edit: false
     }
   }
 
-  handelSubmit(data){
+  handelSubmit(data) {
     this.setState({
       case: data,
-      showCases: false
+      showCases: false,
+      edite: false
     })
   }
 
-  renderShow(data){
+  handelShowEdite(event) {
+    event.preventDefault();
+    this.setState({
+      edit: true
+    })
+  }
+
+  handleSubmit(show) {
+    if(this.state.case) {
+      this.updateShow(show)
+    } else {
+      this.createNewShow(show)
+    }
+  }
+
+  deleteCase(cases) {
+    const url = `http://localhost:3000/lawyers/${cases.id}`;
+    fetch(url, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  updateCase(cases) {
+    const url = `http://localhost:3000/lawyers/${cases.id}`;
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(cases)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+  }
+
+  renderEdit() {
+    return <ShowEdit updateCase={this.updateCase.bind(this)} data={this.state.case} />
+  }
+
+  renderShow(data) {
     console.log(data)
     return (
       <div>
-          <form>
-            <button>Edit</button>
-            <button>Delete</button>
-          </form>
-          <p>Case name: {data.case_name}</p>
-          <p>legal_instruments: {data.legal_instruments}</p>
-          <p>description: {data.description}</p>
-          <p>date: {data.date}</p>
-          <p>prosecultor: {data.prosecultor}</p>
-          <p>defendant: {data.defendant}</p>
-          <p>type: {data.type}</p>
-          <p>corut_name: {data.corut_name}</p>
-          <p>location: {data.location}</p>
+        <div>
+          <button onClick={this.handelShowEdite.bind(this)}>Edit</button>
+          <button onClick={() => this.deleteCase(this.state.case)}>Delete</button>
+        </div>
+        <p>Case name: {data.case_name}</p>
+        <p>legal_instruments: {data.legal_instruments}</p>
+        <p>description: {data.description}</p>
+        <p>date: {data.date}</p>
+        <p>prosecultor: {data.prosecultor}</p>
+        <p>defendant: {data.defendant}</p>
+        <p>type: {data.type}</p>
+        <p>corut_name: {data.corut_name}</p>
+        <p>location: {data.location}</p>
+
+        {this.state.edit ? this.renderEdit() : false}
+
       </div>
     )
   }
 
-  renderCases(){
-   return this.props.data.map((el , index)  => {
+  renderCases() {
+    return this.props.data.map((el, index) => {
       return (
         <div key={index} onClick={() => this.handelSubmit(el)}>
-          <p> Legal Instruments: {el.legal_instruments}</p> 
-          <p> Description: {el.description}</p> 
+          <p> Legal Instruments: {el.legal_instruments}</p>
+          <p> Description: {el.description}</p>
           <p>date: {el.date}</p>
         </div>
-       );
-      })
+      );
+    })
   }
   render() {
     return (
       <div>
-          <h1>Lawyer: {this.props.data[0].lawyer}</h1>
-          <h2> cases </h2>
-          {this.state.showCases ? this.renderCases() : this.renderShow(this.state.case)}
+        <h1>Lawyer: {this.props.data[0].lawyer}</h1>
+        <h2> cases </h2>
+        <button>Create</button>
+        {this.state.showCases ? this.renderCases() : this.renderShow(this.state.case)}
 
-       
+
       </div>
     );
   }
